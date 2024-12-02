afa5a03130bdfbd1e07b8fdc5cea5c5b4f69eaa0
TruongDuke <168114893+TruongDuke@users.noreply.github.com>
2024-12-02 14:40:24 +0700

    commit

---
+++ AimsProjectss/Store.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+
+
+public class Store {
+    public ArrayList<DigitalVideoDisc> itemsInStore = new ArrayList<>();
+
+    public void addDVD(DigitalVideoDisc dvd){
+        itemsInStore.add(dvd);
+        System.out.println("The DVD " + dvd.getTitle() + "has been added.");
+    }
+
+    public void removeDVD(DigitalVideoDisc dvd){
+            if(itemsInStore.remove(dvd)){
+                System.out.println("The DVD " + dvd.toString()+" has been removed.");
+            }
+            else{
+                System.out.println("DVD " + dvd.getTitle() + "not in store" );
+            }
+    }
+
+    public void printStorelist(){
+        System.out.println("\n--- Store ---\n");
+        try {
+            for(DigitalVideoDisc dvd : itemsInStore){
+                System.out.println(dvd.toString() );
+            }
+        } catch (Exception e) {
+        }
+        System.out.println("-------------");
+    }
+
+}
