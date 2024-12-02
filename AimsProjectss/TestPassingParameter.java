11d63786f52240003a9b47f29ee3f87d9bbcadc5
TruongDuke <168114893+TruongDuke@users.noreply.github.com>
2024-12-02 13:57:24 +0700

    commit

---
+++ AimsProjectss/TestPassingParameter.java
@@ -0,0 +1,32 @@
+
+
+public class TestPassingParameter {
+    public static void main(String[] args) {
+        // Tạo hai đối tượng DigitalVideoDisc
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        // Thực hiện swap
+        swap(jungleDVD, cinderellaDVD);
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+
+        // Thực hiện changeTitle
+        changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+    }
+
+    // Phương thức hoán đổi hai đối tượng
+    public static void swap(Object o1, Object o2) {
+        Object tmp = o1;
+        o1 = o2;
+        o2 = tmp;
+    }
+
+    // Phương thức thay đổi tiêu đề
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        dvd = new DigitalVideoDisc(oldTitle);
+    }
+}
